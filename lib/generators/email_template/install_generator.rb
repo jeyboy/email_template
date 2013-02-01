3795bd6c3726c3bae85c7c3dfb8b8d53599763bc
Jenua Boiko <jeyboy@bigmir.net>
2013-02-01 13:53:26 +0200

    add helpers

--- lib/generators/email_template/install_generator.rb
+++ lib/generators/email_template/install_generator.rb
@@ -1,14 +1,20 @@
+require "install_helpers"
+
 module EmailTemplate
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      include EmailTemplate::Generators::OrmHelpers
+
       source_root File.expand_path("../../", __FILE__)
 
       desc "Creates initializer and migration. If ActiveAdmin install - copy admin page for templates"
       class_option :orm
 
       def copy_initializer
-        STDOUT << "migration"
-        template "active_record/migration.rb", "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S").to_i}_AddMailerTemplate.rb"
+        unless migration_exists?("AddMailerTemplate")
+          STDOUT << "migration"
+          template "active_record/migration.rb", "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S").to_i}_AddMailerTemplate.rb"
+        end
 
         STDOUT << "config"
         template "templates/email_templates.rb", "config/initializers/email_template.rb"
@@ -20,6 +26,10 @@ module EmailTemplate
       end
 
       def copy_locale;  end
+
+      def check_migration_exists
+
+      end
     end
   end
 end
---
+++ lib/generators/email_template/install_helpers.rb
@@ -0,0 +1,21 @@
+module EmailTemplate
+  module Generators
+    module OrmHelpers
+      def self.orm
+        Rails::Generators.options[:rails][:orm]
+      end
+
+      def self.orm_has_migration?
+        [:active_record].include? orm
+      end
+
+      def migration_exists?(name)
+        Dir.glob("#{File.join(destination_root, migration_path)}/[0-9]*_*.rb").grep(/\d+_#{name}.rb$/).first
+      end
+
+      def migration_path
+        @migration_path ||= File.join("db", "migrate")
+      end
+    end
+  end
+end
