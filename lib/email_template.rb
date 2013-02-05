d76172ac7b278885f9d73dc36660020ee781aeae
Jenua Boiko <jeyboy@bigmir.net>
2013-02-05 14:05:07 +0200

    clean

--- lib/email_template.rb
+++ lib/email_template.rb
@@ -7,23 +7,12 @@ include JMailers
 include EmailTemplate::Mailers::Helpers
 
 module EmailTemplate
-  #
-  #module Mailers
-  #  autoload :Helpers, 'email_template/mailers/email_template/helpers/helpers'
-  #end
-
   mattr_accessor :columns_black_list
   self.columns_black_list = []
   mattr_accessor :attributes_black_list
   self.attributes_black_list = []
   mattr_accessor :methods_header
   self.methods_header = "et_"
-
-  def self.included(base)
-    self.columns_black_list = []
-    self.attributes_black_list = []
-    self.methods_header = "et_"
-  end
 
   def self.setup
     yield self
