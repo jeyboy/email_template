d9f1618a8eb916c52f938ed88c53c4cefd2cdae2
Ubuntu 12.10 <jb@ubuntu.(none)>
2013-02-06 12:58:55 -0800

    fix devise links

--- lib/email_template/linkable/devise.rb
+++ lib/email_template/linkable/devise.rb
@@ -10,21 +10,30 @@ module Linkable
 
         template_params.reverse_merge!(
           :confirm_link => link_to(opts[:confirm_name] || 'Confirm my account',
-            "#{ActionMailer::Base.default_url_options[:host]}/record.class.name.tableize.singularize/confirmation?confirmation_token=#{@resource.confirmation_token}")
+            "#{link_head(record)}/confirmation?confirmation_token=#{@resource.confirmation_token}")
         ) if @resource.respond_to? :confirmation_token
 
         template_params.reverse_merge!(
           :edit_password_link => link_to(opts[:change_name] || 'Change my password',
-            "#{ActionMailer::Base.default_url_options[:host]}/record.class.name.tableize.singularize/password/edit?reset_password_token=#{@resource.reset_password_token}")
+            "#{link_head(record)}/password/edit?reset_password_token=#{@resource.reset_password_token}")
         ) if @resource.respond_to? :reset_password_token
 
         template_params.reverse_merge!(
           :unlock_link => link_to(opts[:unlock_name] || 'Unlock my account',
-            "#{ActionMailer::Base.default_url_options[:host]}/record.class.name.tableize.singularize/unlock?unlock_token => #{@resource.unlock_token}")
+            "#{link_head(record)}/unlock?unlock_token => #{@resource.unlock_token}")
         ) if @resource.respond_to? :unlock_token
 
-        mailing(check_template(template_name), headers_for(action, mail_params).except(:subject, :template_path, :template_name), template_params.except(:*))
+        mailing(
+            check_template(template_name),
+            headers_for(action, mail_params).except(:subject, :template_path, :template_name),
+            template_params.except(:*)
+        )
       end
+
+      private
+        def link_head(record)
+          "#{ActionMailer::Base.default_url_options[:host]}/#{record.class.name.tableize.singularize}"
+        end
     end
   end
 end
