5c1a24d9bf464eb990e57a91fbb859069f7e639f
Ubuntu 12.10 <jb@ubuntu.(none)>
2013-02-06 13:10:57 -0800

    refactor devise mailer

--- lib/generators/templates/devise/custom_devise_mailer.rb
+++ lib/generators/templates/devise/custom_devise_mailer.rb
@@ -3,17 +3,19 @@ include Linkable::DeviseMailer
 
 class CustomDeviseMailer < DeviseTemplateSendMailer
   def confirmation_instructions(record, opts={})
-    record_name = record.class.name.tableize.singularize
-    send_mail(record, :confirmation_instructions, "#{record_name}_mailer:#{__method__}", opts.merge(record_name.to_sym => record))
+    send_mail(record, :confirmation_instructions, "#{record_name(record)}_mailer:#{__method__}", opts.merge(record_name.to_sym => record))
   end
 
   def reset_password_instructions(record, opts={})
-    record_name = record.class.name.tableize.singularize
-    send_mail(record, :reset_password_instructions, "#{record_name}_mailer:#{__method__}", opts.merge(record_name.to_sym => record))
+    send_mail(record, :reset_password_instructions, "#{record_name(record)}_mailer:#{__method__}", opts.merge(record_name.to_sym => record))
   end
 
   def unlock_instructions(record, opts={})
-    record_name = record.class.name.tableize.singularize
-    send_mail(record, :unlock_instructions, "#{record_name}_mailer:#{__method__}", opts.merge(record_name.to_sym => record))
+    send_mail(record, :unlock_instructions, "#{record_name(record)}_mailer:#{__method__}", opts.merge(record_name.to_sym => record))
+  end
+
+  private
+  def record_name(record)
+    record.class.name.tableize.singularize
   end
 end
