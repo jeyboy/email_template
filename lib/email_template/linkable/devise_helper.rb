f9251f4bf899962cf313122f902c1d0b3819e01a
Jenua Boiko <jeyboy@bigmir.net>
2013-02-07 11:45:16 +0200

    set template and email params fro devise meiler as default - bump

--- lib/email_template/linkable/devise_helper.rb
+++ lib/email_template/linkable/devise_helper.rb
@@ -4,7 +4,7 @@ module Linkable
       "#{ActionMailer::Base.default_url_options[:host]}/#{record.class.name.tableize.singularize}"
     end
 
-    def sending(record, action, template_name, email_opts, template_opts)
+    def sending(record, action, template_name, email_opts = {}, template_opts = {})
       send_mail(record, action, template_name, email_opts,
                 template_opts.merge(obj_class_name(record).to_sym => record))
     end
